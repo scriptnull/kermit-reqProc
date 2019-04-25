08bbd4ce100691d3aee034c55bdcdeb5a18026c5
ankul <ankul@shippable.com>
2019-04-25 18:52:26 +0530

    add auto order in reqproc assemblyOrder

--- assembler/assemble.js
+++ assembler/assemble.js
@@ -15,7 +15,7 @@ var isDirectory = require('../_common/helpers/isDirectory.js');
 var isFile = require('../_common/helpers/isFile.js');
 
 var assemblyOrder = ['onSuccess', 'onFailure', 'onComplete',
-  'environmentVariables', 'image', 'onStart', 'onExecute'];
+  'environmentVariables', 'image', 'auto', 'onStart', 'onExecute'];
 
 function assemble(externalBag, callback) {
   var bag = {
