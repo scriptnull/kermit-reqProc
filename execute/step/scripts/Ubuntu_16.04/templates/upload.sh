c54232bf719995499825f6bb1c48949b4f97319b
Devashish <devashish.86@gmail.com>
2019-05-17 04:21:43 +0000

    testing code

--- execute/step/scripts/Ubuntu_16.04/templates/upload.sh
+++ execute/step/scripts/Ubuntu_16.04/templates/upload.sh
@@ -10,6 +10,8 @@ export STEP_WORKSPACE_DIR="%%stepWorkspaceDir%%"
 export RUN_WORKSPACE_DIR="%%runWorkspaceDir%%"
 
 upload_step_artifacts() {
+  echo "-----------testing step state ----------------"
+  env
   if [ -z "$STEP_ARTIFACT_URL" ]; then
     echo "No step artifact storage available."
     return 0
@@ -44,6 +46,8 @@ upload_step_artifacts() {
 }
 
 upload_run_state() {
+  echo "-----------testing run state----------------"
+  env
   if [ -z "$RUN_ARTIFACT_URL" ]; then
     echo "No run state storage available."
     return 0
