5d7ba9b855c63863ca6362069240cd4bfe724573
Vishnu Bharathi <vishnubharathi04@gmail.com>
2019-05-21 15:08:49 +0530

    adds logging

--- execute/step/createStepletScript.js
+++ execute/step/createStepletScript.js
@@ -169,6 +169,8 @@ function _assembleScript(bag, next) {
 
       bag.stepConsoleAdapter.publishMsg(
         'Successfully assembled steplet script');
+      bag.stepConsoleAdapter.publishMsg('mylog result.assembledScript = ' +
+        result.assembledScript);
       bag.stepConsoleAdapter.closeCmd(true);
       bag.assembledScript = result.assembledScript;
       return next();
