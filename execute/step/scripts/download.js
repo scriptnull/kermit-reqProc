cbaea76c3be9ccbd59d68150c95c1b4c55a8bd8e
Aidan Murphy <aidanm@jfrog.com>
2019-05-23 21:15:32 -0700

    #205; changes pipeline state artifact names.

--- execute/step/scripts/download.js
+++ execute/step/scripts/download.js
@@ -26,8 +26,6 @@ function download(externalBag, callback) {
     runWorkspacePath: externalBag.runWorkspacePath,
     pipelineArtifactUrl: externalBag.pipelineArtifactUrl,
     pipelineArtifactUrlOpts: externalBag.pipelineArtifactUrlOpts,
-    pipelineArtifactHeadUrl: externalBag.pipelineArtifactHeadUrl,
-    pipelineArtifactHeadUrlOpts: externalBag.pipelineArtifactHeadUrlOpts,
     pipelineArtifactName: externalBag.pipelineArtifactName,
     pipelineWorkspacePath: externalBag.pipelineWorkspacePath,
     builderApiAdapter: externalBag.builderApiAdapter,
@@ -108,8 +106,6 @@ function _generateScript(bag, next) {
     runWorkspaceDir: bag.runWorkspacePath,
     pipelineArtifactUrl: bag.pipelineArtifactUrl,
     pipelineArtifactUrlOpts: bag.pipelineArtifactUrlOpts,
-    pipelineArtifactHeadUrl: bag.pipelineArtifactHeadUrl,
-    pipelineArtifactHeadUrlOpts: bag.pipelineArtifactHeadUrlOpts,
     pipelineArtifactName: bag.pipelineArtifactName,
     pipelineWorkspaceDir: bag.pipelineWorkspacePath
   };
