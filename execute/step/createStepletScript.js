4b9eeaf0fe996eac64897b25aafa50c476375fdf
Deepika Banoth <deepikabanoth@deepikab-mac.local>
2019-06-05 15:26:04 +0530

    rename PIPELINE_WORKSPACE_DIR to pipeline_workspace_dir

--- execute/step/createStepletScript.js
+++ execute/step/createStepletScript.js
@@ -108,7 +108,7 @@ function _setScriptEnvs(bag, next) {
       'step_dependency_state_dir': bag.dependencyStateDir,
       'step_output_dir': bag.outputDir,
       'step_workspace_dir': bag.stepWorkspacePath,
-      'PIPELINE_WORKSPACE_DIR': bag.pipelineWorkspacePath,
+      'pipeline_workspace_dir': bag.pipelineWorkspacePath,
       'STEP_TMP_DIR': path.join(bag.stepWorkspacePath, 'tmp'),
       'OPERATING_SYSTEM': global.config.shippableNodeOperatingSystem,
       'ARCHITECTURE': global.config.shippableNodeArchitecture,
